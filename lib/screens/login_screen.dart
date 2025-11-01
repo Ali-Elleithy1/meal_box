fbcf24872432fceb5bc11eb11e84033bdabf913d
Ali-Elleithy1 <ali.kh1615@gmail.com>
2025-11-01 11:36:28 +0200

    Add fields and buttons in login screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -6,22 +6,180 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(context) {
     return Scaffold(
-      body: Center(
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          mainAxisSize: MainAxisSize.max,
           children: [
-            Text('Login', style: TextStyle(fontSize: 24)),
             SizedBox(height: 48),
-            Form(
-              child: Column(
-                children: [
-                  TextFormField(
-                    decoration: InputDecoration(label: Text('Email/Username')),
+            Padding(
+              padding: const EdgeInsets.only(left: 16.0),
+              child: Text(
+                'Login',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xFF302f67),
+                ),
+              ),
+            ),
+            SizedBox(height: 36),
+            Expanded(
+              child: Container(
+                margin: EdgeInsets.only(bottom: 16),
+                padding: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  color: Color(0xFF302f67),
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(60),
+                    bottomLeft: Radius.circular(33),
+                    bottomRight: Radius.circular(33),
                   ),
-                  TextFormField(
-                    decoration: InputDecoration(label: Text('Password')),
+                ),
+                width: double.infinity,
+                child: Form(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      SizedBox(height: 48),
+                      TextFormField(
+                        keyboardType: TextInputType.emailAddress,
+                        style: TextStyle(color: Colors.white),
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.person),
+                          prefixIconColor: Colors.white,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12),
+                            borderSide: BorderSide(
+                              color: Colors.white,
+                              width: 2,
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12),
+                            borderSide: BorderSide(
+                              color: Colors.white,
+                              width: 2,
+                            ),
+                          ),
+                          label: Text(
+                            'Email/Username',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                      TextFormField(
+                        obscureText: true,
+                        style: TextStyle(color: Colors.white),
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(Icons.password),
+                          prefixIconColor: Colors.white,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12),
+                            borderSide: BorderSide(color: Colors.white),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12),
+                            borderSide: BorderSide(
+                              color: Colors.white,
+                              width: 2,
+                            ),
+                          ),
+                          label: Text(
+                            'Password',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            'Forgot Password?',
+                            style: TextStyle(
+                              decoration: TextDecoration.underline,
+                              decorationColor: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 24),
+                      ElevatedButton.icon(
+                        style: ElevatedButton.styleFrom(
+                          iconColor: Colors.white,
+                          minimumSize: Size(300, 50),
+                          backgroundColor: Color(0xFF6a57f7),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                        ),
+                        onPressed: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(builder: (ctx) => LoginScreen()),
+                          );
+                        },
+                        label: Text(
+                          'Continue with Email',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        icon: Icon(Icons.mail),
+                      ),
+                      SizedBox(height: 8),
+                      ElevatedButton.icon(
+                        style: ElevatedButton.styleFrom(
+                          minimumSize: Size(300, 50),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                        ),
+                        onPressed: () {},
+                        label: Text('Continue with Google'),
+                        icon: Image.asset(
+                          'assets/images/google.png',
+                          width: 24,
+                          height: 24,
+                        ),
+                      ),
+                      SizedBox(height: 48),
+                      Align(
+                        alignment: Alignment.centerLeft,
+                        child: TextButton(
+                          onPressed: () {},
+                          child: TextButton(
+                            onPressed: () {},
+                            child: Text.rich(
+                              TextSpan(
+                                text: 'Already have an account? ',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 12,
+                                ),
+                                children: <TextSpan>[
+                                  TextSpan(
+                                    text: 'Login',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 14,
+                                      decoration: TextDecoration.underline,
+                                      decorationColor: Colors.white,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
-                ],
+                ),
               ),
             ),
           ],
