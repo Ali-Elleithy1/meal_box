7435d8fcbcf7de5cb30e360dd45d69c5902c8dd0
Ali Elleithy <ali.kh1615@gmail.com>
2025-11-02 13:04:31 +0200

    Add fields and buttons in register form

--- lib/screens/sign_up_screen.dart
+++ lib/screens/sign_up_screen.dart
@@ -9,7 +9,7 @@ class SignUpScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(),
       body: Padding(
-        padding: const EdgeInsets.all(48.0),
+        padding: const EdgeInsets.all(36.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
@@ -60,6 +60,70 @@ class SignUpScreen extends StatelessWidget {
               textColor: Color(0xFF302f67),
               prefixIconColor: Color(0xFF302f67),
               borderColor: Colors.black,
+            ),
+            const SizedBox(height: 24),
+            Row(
+              children: [
+                Checkbox(value: false, onChanged: null),
+                SizedBox(width: 8),
+                Text('By clicking you agree to '),
+                Text(
+                  'terms & conditions',
+                  style: TextStyle(
+                    color: Colors.blueAccent,
+                    decoration: TextDecoration.underline,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 36),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                minimumSize: Size(double.infinity, 50),
+                backgroundColor: Color(0xFF6a57f7),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                ),
+              ),
+              onPressed: () {},
+              child: Text('Register', style: TextStyle(color: Colors.white)),
+            ),
+            Spacer(),
+            Container(
+              decoration: BoxDecoration(
+                color: Color(0xFF302f67),
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(60),
+                  bottomLeft: Radius.circular(33),
+                  bottomRight: Radius.circular(33),
+                ),
+              ),
+              width: double.infinity,
+              height: 200,
+              child: TextButton(
+                onPressed: () {},
+                child: TextButton(
+                  onPressed: () {},
+                  child: Text.rich(
+                    TextSpan(
+                      text: 'Already have an account? ',
+                      style: TextStyle(color: Colors.white, fontSize: 12),
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: 'Login',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 14,
+                            decoration: TextDecoration.underline,
+                            decorationColor: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
             ),
           ],
         ),
